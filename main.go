420d2ad533d469db40cb764a2d5c463b984d3aa8
ichtrojan <ordrizzy@gmail.com>
2020-04-23 03:07:13 +0100

    main plug

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/ichtrojan/thoth"
+	"github.com/vergly/payment/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	api := routes.Api()
+
+	logger, err := thoth.Init("log")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := http.ListenAndServe(":9090", api); err != nil {
+		logger.Log(err)
+	}
+}
